52cb536caf9a9978165daf941a62545e738591ba
hnpanther <hadi.hnp@gmail.com>
2020-08-24 22:49:55 +0430

    add .mp4 to .gitignore create video.html template for play video autowired videoFileService for VideoFileController add "/player" url for video.html

--- src/main/java/ir/hadi/springbootvideostreaming/controller/VideoFileController.java
+++ src/main/java/ir/hadi/springbootvideostreaming/controller/VideoFileController.java
@@ -1,11 +1,27 @@
 package ir.hadi.springbootvideostreaming.controller;
 
+import ir.hadi.springbootvideostreaming.service.VideoFileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/video")
 public class VideoFileController {
+
+    private VideoFileService videoFileService;
+
+    public VideoFileController(@Autowired  VideoFileService videoFileService) {
+        this.videoFileService = videoFileService;
+    }
+
+    @GetMapping("/player")
+    public String getVideo() {
+        return "video";
+    }
+
+
 
 
 }
