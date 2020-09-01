29c1a393741c1f99dcc1aa1927ffbc685bf3add9
hnpanther <hadi.hnp@gmail.com>
2020-09-01 13:09:13 +0430

    create exception package and CustomErrorResponse class create RestEndpointExceptionHandler for handle ClientAbortException(occour in download video file with idm)

---
+++ src/main/java/ir/hadi/springbootvideostreaming/exception/CustomErrorResponse.java
@@ -0,0 +1,55 @@
+package ir.hadi.springbootvideostreaming.exception;
+
+import java.time.LocalDateTime;
+
+
+
+public class CustomErrorResponse {
+
+    String errorCode;
+    String errorMsg;
+    int status;
+
+    LocalDateTime timestamp;
+
+    public CustomErrorResponse(String errorCode, String errorMsg) {
+        super();
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+
+}
